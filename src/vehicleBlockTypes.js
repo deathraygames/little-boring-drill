0dcacd2abf3da98eb371ad8c3bcec879756da7b8
Luke <luke.nickerson@gmail.com>
2021-05-05 23:31:09 -0400

    Improved build UI

--- src/vehicleBlockTypes.js
+++ src/vehicleBlockTypes.js
@@ -16,14 +16,15 @@ const vehicleBlockTypes = Object.freeze({
 		powerCapacity: 1,
 	},
 	// Assorted
-	"copper-hull": { cost: { "copper-parts": 2 }, category: 'hull' },
-	"iron-hull": { cost: { "iron-beams": 4 }, category: 'hull' },
-	"aluminum-hull": { cost: { "aluminum-plates": 4 }, category: 'hull' },
+	"copper-hull": { cost: { "copper-parts": 2 }, category: 'hull', tier: 1 },
+	"iron-hull": { cost: { "iron-beams": 4 }, category: 'hull', tier: 2 },
+	"aluminum-hull": { cost: { "aluminum-plates": 4 }, category: 'hull', tier: 3 },
 	"mini-dome": {
 		squareSize: 10,
 		cost: { "aluminum-plates": 5 },
 		connections: [[0, 1]],
 		lifeSupport: 1,
+		tier: 3,
 	},
 	"survival-core": {
 		category: 'production',
@@ -36,17 +37,20 @@ const vehicleBlockTypes = Object.freeze({
 		refining: 1,
 		printing: 1,
 		pulling: 1,
+		tier: 5
 	},
 	"copper-structure-h": {
 		category: 'hull',
-		cost: { "copper-parts": 2 }
+		cost: { "copper-parts": 2 },
+		tier: 1
 	},
 	"copper-structure-v": {
 		category: 'hull',
-		cost: { "copper-parts": 2 }
+		cost: { "copper-parts": 2 },
+		tier: 1
 	},
-	"iron-structure-h": { cost: { "iron-beams": 4 }, category: 'hull' },
-	"iron-structure-v": { cost: { "iron-beams": 4 }, category: 'hull' },
+	"iron-structure-h": { cost: { "iron-beams": 4 }, category: 'hull', tier: 2 },
+	"iron-structure-v": { cost: { "iron-beams": 4 }, category: 'hull', tier: 2 },
 
 	// Drills
 	"small-copper-drill": {
@@ -56,6 +60,7 @@ const vehicleBlockTypes = Object.freeze({
 		connections: [[0, -1]],
 		cargoSpace: 1,
 		drilling: 1,
+		tier: 1,
 	},
 	"copper-drill": {
 		category: 'drills',
@@ -63,6 +68,7 @@ const vehicleBlockTypes = Object.freeze({
 		connections: [[0, -1]],
 		cargoSpace: 2,
 		drilling: 2,
+		tier: 1,
 	},
 	"iron-drill": {
 		category: 'drills',
@@ -70,6 +76,7 @@ const vehicleBlockTypes = Object.freeze({
 		connections: [[0, -1]],
 		cargoSpace: 3,
 		drilling: 3,
+		tier: 2,
 	},
 	"aluminum-drill": {
 		category: 'drills',
@@ -77,20 +84,23 @@ const vehicleBlockTypes = Object.freeze({
 		connections: [[0, -1]],
 		cargoSpace: 5,
 		drilling: 4,
+		tier: 3,
 	},
 	"titanium-drill": {
 		category: "drills",
 		cost: { "titanium-parts": 30 },
 		connections: [[0, -1]],
 		cargoSpace: 8,
-		drilling: 5,	
+		drilling: 5,
+		tier: 4,
 	},
 	"obsidian-drill": {
 		category: "drills",
 		cost: { "obsidian-parts": 30 },
 		connections: [[0, -1]],
 		cargoSpace: 10,
-		drilling: 6,	
+		drilling: 6,
+		tier: 5,
 	},
 	"aluminum-cargo-drill": {
 		category: 'drills',
@@ -99,6 +109,7 @@ const vehicleBlockTypes = Object.freeze({
 		connections: [[0, -1]],
 		cargoSpace: 10,
 		drilling: 1,
+		tier: 3,
 	},
 
 	// Cargo
@@ -108,6 +119,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 10,
 		pulling: 1,
 		pulls: 'printed',
+		tier: 1,
 	},
 	"iron-ore-cargo": {
 		category: "cargo",
@@ -115,6 +127,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 10,
 		pulling: 1,
 		pulls: 'ore',
+		tier: 2,
 	},
 	"aluminum-cargo": {
 		category: "cargo",
@@ -122,6 +135,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 20,
 		pulling: 2,
 		pulls: 'printed',
+		tier: 3,
 	},
 	"titanium-cargo": {
 		category: "cargo",
@@ -129,6 +143,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 30,
 		pulling: 4,
 		pulls: 'printed',
+		tier: 4,
 	},
 	"obsidian-cargo": {
 		category: "cargo",
@@ -136,6 +151,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 40,
 		pulling: 5,
 		pulls: 'printed',
+		tier: 5,
 	},
 
 	// Refineries
@@ -145,6 +161,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 2,
 		refining: 1,
 		pulling: 1,
+		tier: 1,
 	},
 	"iron-refinery": {
 		category: "production",
@@ -152,6 +169,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 3,
 		refining: 4,
 		pulling: 1,
+		tier: 2,
 	},
 	"titanium-refinery": {
 		category: "production",
@@ -159,6 +177,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 5,
 		refining: 5,
 		pulling: 2,
+		tier: 4,
 	},
 	"obsidian-refinery": {
 		category: "production",
@@ -166,6 +185,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 6,
 		refining: 7,
 		pulling: 2,
+		tier: 5,
 	},
 
 	// Printers
@@ -175,6 +195,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 2,
 		printing: 1,
 		pulling: 1,
+		tier: 1,
 	},
 	"aluminum-printer": {
 		category: "production",
@@ -182,6 +203,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 4,
 		printing: 3,
 		pulling: 2,
+		tier: 3,
 	},
 	"titanium-printer": {
 		category: "production",
@@ -189,6 +211,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 6,
 		printing: 4,
 		pulling: 4,
+		tier: 4,
 	},
 	"obsidian-printer": {
 		category: "production",
@@ -196,6 +219,7 @@ const vehicleBlockTypes = Object.freeze({
 		cargoSpace: 7,
 		printing: 5,
 		pulling: 4,
+		tier: 5,
 	},
 
 	// "rotation-test": {
@@ -214,7 +238,9 @@ const vehicleBlockTypes = Object.freeze({
 	},
 	forEach(callback) {
 		const excludes = ['get', 'default', 'forEach'];
-		const keys = Object.keys(this).filter((key) => !excludes.includes(key));
+		const keys = Object.keys(this)
+			.filter((key) => !excludes.includes(key))
+			.sort((a, b) => this[a].tier - this[b].tier);
 		keys.forEach((key) => callback(this.get(key)));
 	}
 });
